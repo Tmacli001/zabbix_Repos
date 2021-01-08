d1efca33b010bc1be2f5e81a4bfc58e05546338a
Tmacli001 <65435545+Tmacli001@users.noreply.github.com>
2021-01-08 18:13:36 +0800

    Update zabbix-server-deployment.sh
    update zabbix_server_install.sh

--- zabbix-server-deployment.sh
+++ zabbix-server-deployment.sh
@@ -2,6 +2,7 @@
 #Author polo.li
 #Email 945626422@qq.com
 #Description zabbix-setup-script
+#insible to centos7-8 os
 #Create time 2020-05-30 14:20
 
 echo -e "\033[40;36m zabbix server's env mysql+nginx+php+zabbix-server....... \033[0m"
@@ -22,8 +23,10 @@ gate_way=10.0.20.254
 internet_dns=114.114.114.114
 intranet_dns=10.0.1.9
 echo os_version
+
 #修改服务器的主机名称
 hostnamectl set-hostname www.xwzjk.com
+
 #主机网络配置
 cp /etc/sysconfig/network-scripts/ifcfg-ens33 /etc/sysconfig/network-scripts/ifcfg-ens33.bak
 cat > /etc/sysconfig/network-scripts/ifcfg-ens33 <<EOF
@@ -39,14 +42,17 @@ GATEWAY=$gate_way
 DNS1=$internet_dns
 DNS2=$intranet_dns
 EOF
+
 #restart host network service
 service network restart
 echo "host network is restarting ....."
 sleep 3
-#1.给最小化的系统安装相应的文件软件包或依赖库包
+
+#1.给最小化的系统安装相应的应用软件包和依赖库包
 yum install epel-release net-tools htop telnet curl zip tar wget git gcc expect centos-release-scl -y
 yum install net-snmp-devel libxml2-devel libcurl-devel libevent libevent-devel -y
 sleep 9
+
 #2.install all need's packages for mysql/postgresql database or nginx/apache server
 cd /usr/local/src
 wget http://nginx.org/download/nginx-1.16.1.tar.gz
@@ -55,12 +61,14 @@ cd  nginx-1.16.1
 ./configure  --prefix=/usr/local/web_server/nginx --with-http_stub_status_module --with-http_ssl_module
 make && make install
 /usr/local/web_server/nginx/sbin/nginx -v
+
 #nginx setup create user and group
 /usr/sbin/groupadd www
 /usr/sbin/useradd -g www www
 #/usr/local/web_server/nginx/sbin/nginx
 
 #3.nginx configure file
+
 #4.install php
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webstatic-release.rpm
 sudo yum install -y php72w-fpm php72w-cli php72w-gd php72w-mbstring php72w-mysqlnd php72w-opcache php72w-pdo php72w-xml php72w-process
@@ -77,7 +85,8 @@ mysql="/usr/bin/mysql -uroot"
 mylogs='/var/log/mysqld.log'
 cat $mylogs|grep 'temporary password'|awk '{print $NF}'|head -1 > /root/mysqlpwd.txt
 passwd=`cat /root/mysqlpwd.txt`
-#安装expect预期交互
+
+#应用expect预期交互修改mysql数据库root密码
 if [ $? -eq 0 ];then
 {
 expect <<EOF
@@ -86,19 +95,22 @@ expect "Enter password:" {send "$passwd\r"}
 expect "mysql>" {send "SET PASSWORD FOR 'root'@'localhost'='Polo.li123*6';show databases;\r"}
 expect "mysql>" {send "quit\r"}
 EOF
+
 echo "please wait a moment ................."
 echo "mysql 数据库密码已经完成重置动作......"
 echo "......................................"
 New_password='Polo.li123*6'
+
 #A.获取MySQL原始密码
 #cat $mylogs|grep 'temporary password'|awk  ‘{print $NF}’|head –1 > /root/mysqlpassword.txt
 #passwd=`cat /root/mysqlpassword.txt`
+
 #登录MySQL 修改原始密码
 #$mysql -p$passwd << EOF
 #SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Polo.li123*6');
 #CREATE DATABASE service_chat DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 #EOF
-#用新变量来存储新密码
+
 #B.创建初始数据库 Run the following on your database host.
 #mysql -uroot -p$New_password << EOF
 #create database zabbix character set utf8 collate utf8_bin;
@@ -106,12 +118,14 @@ New_password='Polo.li123*6'
 #grant all privileges on zabbix.* to zabbix@localhost;
 #quit;
 #EOF
+
 expect <<EOF
 spawn sudo $mysql -p
 expect "Enter password:" {send "$New_password\r"}
 expect "mysql>" {send " create database zabbix character set utf8 collate utf8_bin;create user zabbix@localhost identified by 'Zabbix_001';grant all privileges on zabbix.* to zabbix@localhost;\r"}
 expect "mysql>" {send "quit\r"}
 EOF
+
 echo "zabbix database created successful and setup has finished ....."
 }
 else
@@ -121,6 +135,7 @@ exit 0
 echo "halo world ,please check your operator, thank you !!!"
 }
 fi
+
 #6.firewalld I/O port setup
 sed -ri '/^SELINUX=/cSELINUX=disabled' /etc/selinux/config
 #systemctl stop firewalld
@@ -132,28 +147,34 @@ if [ -f zabbix-release-5.0.* ];then
 	   echo "zabbix packages has exist,please check is or not continue operator !!!"
        exit 0
 else
-{  
-function zabbix_install() {
+{
+
+function zabbix_server_install() {
 #1.安装zabbix的yum源
 rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
 yum clean all
+
 #2.编辑zabbix源配置文件 
 #/etc/yum.repos.d/zabbix.repo and enable zabbix-frontend repository.
 #sed -ri '/^enabled=/cenabled=1' /etc/yum.repos.d/zabbix.repo
 nl /etc/yum.repos.d/zabbix.repo |sed -i '11d' /etc/yum.repos.d/zabbix.repo
 nl /etc/yum.repos.d/zabbix.repo |sed -i '10a enabled=1'  /etc/yum.repos.d/zabbix.repo
 yum makecache &\
+
 #3.安装zabbix-server and agent
 yum install zabbix-server-mysql zabbix-agent -y
+
 #4.Install Zabbix frontend packages.
 yum install zabbix-web-mysql-scl zabbix-nginx-conf-scl -y
 Zabbix_pwd='Zabbix_001'
+
 #5.导入初始架构和数据，系统将提示您输入新创建的密码。
 expect <<EOF
 spawn sudo zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -p zabbix
 expect "Enter password:" {send "$Zabbix_pwd\r"}
 expect "mysql>" {send "quit\r"}
 EOF
+
 #6.编辑配置文件 /etc/zabbix/zabbix_server.conf
 cp /etc/zabbix/zabbix_server.conf /etc/zabbix/zabbix_server.conf.bak
 #cat >> /etc/zabbix/zabbix_server.conf <<EOF need alter config information
@@ -176,6 +197,7 @@ sed -ri '/^#DBSocket=/cDBSocket=/var/lib/mysql/mysql.sock'   /etc/zabbix/zabbix_
 #listen 80;
 #server_name www.xwzjk.com,10.0.20.188;
 #EOF
+
 cp /etc/opt/rh/rh-nginx116/nginx/conf.d/zabbix.conf /etc/opt/rh/rh-nginx116/nginx/conf.d/zabbix.conf.bak
 sed -ri '/^#listen 80;/clisten 80;'  /etc/opt/rh/rh-nginx116/nginx/conf.d/zabbix.conf
 sed -ri '/^#server_name;/cserver_name www.xwzjk.com;'  /etc/opt/rh/rh-nginx116/nginx/conf.d/zabbix.conf
@@ -187,6 +209,7 @@ sed -ri '/^; php_value[date.timezone]/cphp_value[date.timezone] = Asia/shanghai'
 #listen.acl_users = apache,nginx
 #php_value[date.timezone] = Asia/shanghai
 #EOF
+
 #8.firewalld zabbix port config
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --zone=public --add-port=10050/tcp --permanent
@@ -201,11 +224,13 @@ systemctl enable zabbix-server zabbix-agent rh-nginx116-nginx rh-php72-php-fpm
 }
 zabbix_install > /root/my_zabbix_install.log
 echo "my zabbix controller is installed !!!"
+
 }
 fi
+
 echo “ you are welcome use zabbix auto-install script !!! ”
 echo " end ............................."
-
+#script end ......
 
 
 
